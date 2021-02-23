48544c807adf327e822c68e7f68d9f478fa88a42
Agnes <51322389+yuchen0531@users.noreply.github.com>
2021-02-23 17:14:25 +0800

    Add files via upload

--- static/js/app-v20210114-c-v2.js
+++ static/js/app-v20210114-c-v2.js
@@ -444,7 +444,7 @@ var Rule_Component = Rule_normalizeComponent(
       playing: false,
       finished: false,
       level: 'normal',
-      disappearspeed: 800,
+      disappearspeed: 500,
       addspeed: 400,
       countplay: null,
       counttime: 3,
@@ -540,14 +540,14 @@ var Rule_Component = Rule_normalizeComponent(
     }
   }
 });
-// CONCATENATED MODULE: ./node_modules/vue-loader/lib/template-compiler?{"id":"data-v-2f39df53","hasScoped":false,"transformToRequire":{"video":["src","poster"],"source":"src","img":"src","image":"xlink:href"},"buble":{"transforms":{}}}!./node_modules/vue-loader/lib/selector.js?type=template&index=0!./src/views/Game.vue
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/template-compiler?{"id":"data-v-58784b76","hasScoped":false,"transformToRequire":{"video":["src","poster"],"source":"src","img":"src","image":"xlink:href"},"buble":{"transforms":{}}}!./node_modules/vue-loader/lib/selector.js?type=template&index=0!./src/views/Game.vue
 var Game_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"game d-flex align-items-center"},[_c('div',[_c('div',{staticClass:"text-white h8 text-center mb-2"},[_vm._v("Time: "+_vm._s(_vm.time)+" Score: "+_vm._s(_vm.score)+" version2")]),_vm._v(" "),_c('div',{staticClass:"game-content d-flex flex-wrap w-85 mx-auto mb-3"},_vm._l((9),function(items,index){return _c('div',{key:index,staticClass:"game-items m-2 bg-white position-relative"},[_c('transition',{attrs:{"name":"fade"}},[_c('div',{staticClass:"d-none position-absolute game-click-item",attrs:{"id":("game_item_" + index)},on:{"click":function($event){return _vm.getScore(index)}}},[_c('img',{attrs:{"src":__webpack_require__("bcBx"),"width":"100%","alt":""}})])]),_vm._v(" "),_vm._m(0,true)],1)}),0),_vm._v(" "),(!_vm.playing)?_c('div',{staticClass:"d-flex justify-content-center mb-3"},[_c('div',{staticClass:"text-white"},[_c('input',{directives:[{name:"model",rawName:"v-model",value:(_vm.level),expression:"level"}],attrs:{"type":"radio","id":"easy","value":"easy"},domProps:{"checked":_vm._q(_vm.level,"easy")},on:{"change":function($event){_vm.level="easy"}}}),_vm._v(" "),_c('label',{attrs:{"for":"easy"}},[_vm._v("easy")]),_vm._v(" "),_c('input',{directives:[{name:"model",rawName:"v-model",value:(_vm.level),expression:"level"}],attrs:{"type":"radio","id":"normal","value":"normal"},domProps:{"checked":_vm._q(_vm.level,"normal")},on:{"change":function($event){_vm.level="normal"}}}),_vm._v(" "),_c('label',{attrs:{"for":"normal"}},[_vm._v("normal")]),_vm._v(" "),_c('input',{directives:[{name:"model",rawName:"v-model",value:(_vm.level),expression:"level"}],attrs:{"type":"radio","id":"difficult","value":"difficult"},domProps:{"checked":_vm._q(_vm.level,"difficult")},on:{"change":function($event){_vm.level="difficult"}}}),_vm._v(" "),_c('label',{attrs:{"for":"difficult"}},[_vm._v("difficult")])])]):_vm._e(),_vm._v(" "),_c('div',{staticClass:"start-btn p-3 text-white mx-auto",on:{"click":_vm.start}},[_vm._v("start")])]),_vm._v(" "),(_vm.finished)?_c('div',{staticClass:"model d-flex align-items-center justify-content-center"},[_c('div',[_c('div',{staticClass:"text-white h8 text-center mb-5"},[_vm._v("Your Score : "+_vm._s(_vm.score))]),_vm._v(" "),_c('div',{staticClass:"start-btn p-3 text-white mx-auto",on:{"click":_vm.gamefinish}},[_vm._v("close")])])]):_vm._e(),_vm._v(" "),(_vm.count)?_c('div',{staticClass:"model d-flex align-items-center justify-content-center"},[_c('div',[_c('div',{staticClass:"text-white h8 text-center mb-5"},[_vm._v(_vm._s(_vm.counttime))])])]):_vm._e()])}
 var Game_staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{},[_c('img',{attrs:{"src":__webpack_require__("9q15"),"width":"100%","alt":""}})])}]
 var Game_esExports = { render: Game_render, staticRenderFns: Game_staticRenderFns }
 /* harmony default export */ var views_Game = (Game_esExports);
 // CONCATENATED MODULE: ./src/views/Game.vue
 function Game_injectStyle (ssrContext) {
-  __webpack_require__("TGDr")
+  __webpack_require__("x77I")
 }
 var Game_normalizeComponent = __webpack_require__("VU/8")
 /* script */
@@ -643,13 +643,6 @@ new vue_esm["a" /* default */]({
 
 /***/ }),
 
-/***/ "TGDr":
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-
 /***/ "UffO":
 /***/ (function(module, exports) {
 
@@ -679,6 +672,13 @@ module.exports = __webpack_require__.p + "static/img/u-money.28bf8d2.png";
 /***/ }),
 
 /***/ "o8nZ":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "x77I":
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
